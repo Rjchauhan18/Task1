adca95cb398fc455fe99ccb830b1cedd8a0f0078
Rjchauhan18 <rjchauhan5000000@gmail.com>
2025-01-15 20:13:45 +0530

    added readme.md to guide for intialization with repositery

--- project/project/settings.py
+++ project/project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q21(e!-w8eh^5))^#_3di^u-rruu3qtail8u!^_70wz!ro=25r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['task1-la4g.onrender.com', '0.0.0.0']
+ALLOWED_HOSTS = ['task1-la4g.onrender.com', '0.0.0.0','127.0.0.1']
 
 
 # Application definition
